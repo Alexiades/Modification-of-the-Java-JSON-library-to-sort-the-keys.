8cc0eb197129a69ef1e2ed67b047c6038f611f76
Alejandro Alexiades <alejandro_alexiades@bnetbuilders.com>
2018-07-29 11:04:13 +0200

    - Example of how the modificated library it's works

---
+++ maven/src/main/java/main.java
@@ -0,0 +1,43 @@
+//original Java json.simple library
+//import org.json.simple.JSONObject;
+//My modificated Java json.simple library for order the fields
+
+
+import org.json.simple.JSONObject;
+
+/****************************************************
+ *  Author: Alejandro Alexiades                     *
+ *                                                  *
+ *  Example of the modificated Json.Simple Libary   *
+ *  for order the fields of the Json that comes     *
+ *  from an array.                                  *
+ *                                                  *
+ ***************************************************/
+
+public class main {
+
+    public static void main( String[] arg ){
+
+        String[] header={"Name","Surname","Country","State","City", "Address","PostalCode"};
+        String[] content={"Alex","Alexiades","Tazmania","Taz","DiabloCity", "Crazy Avenue","9999"};
+
+
+        //Test Json library
+        System.out.println(Order(header,content));
+    }
+
+
+    public static JSONObject Order(String[] hdr,String[] cnt){
+        
+        JSONObject exOrd= new JSONObject();
+
+        for (int index=0;index<hdr.length;index++){
+
+            final Object put = exOrd.put(hdr[index], cnt[index]);
+
+        }
+
+        return exOrd;
+    }
+
+}
